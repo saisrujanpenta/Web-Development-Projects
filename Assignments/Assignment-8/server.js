0b612db3ea810f3c685ca4f292daa4a80e1a912c
saisrujanpenta <penta.s@northeastern.edu>
2022-11-11 18:34:45 -0500

    defined validations

--- Assignments/Assignment-8/server.js
+++ Assignments/Assignment-8/server.js
@@ -1,3 +1,5 @@
+const { validateEmail, checkPassword } = require("./validations/validations");
+
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
