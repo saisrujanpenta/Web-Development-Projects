7ea6539a9700cf6225062d822d4fabf83a53c4ff
saisrujanpenta <penta.s@northeastern.edu>
2022-11-11 12:06:58 -0500

    connected mongo db to node

---
+++ Assignments/Assignment-8/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.log(err)
+})
+
+db.once('open', () => {
+    console.log('Database Connection Established!')
+})
