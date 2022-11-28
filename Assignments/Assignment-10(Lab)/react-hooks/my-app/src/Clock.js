3f6f58118888b8fa6c6774bf2fea943790d69a59
saisrujanpenta <penta.s@northeastern.edu>
2022-11-28 16:32:32 -0500

    react hooks complete

--- Assignments/Assignment-10(Lab)/react-hooks/my-app/src/Clock.js
+++ Assignments/Assignment-10(Lab)/react-hooks/my-app/src/Clock.js
@@ -3,10 +3,22 @@ import React, {useState, useEffect} from 'react'
 const Clock = () => {
 
     const [date, setDate] = useState(new Date());
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        setInterval(tick(), 1000);
+    }, [date]);
+
+    const tick = () => {
+        setDate(new Date());
+    }
+
   return (
     <div>
         <h1>Hello World!</h1>
         <h2>{date.toLocaleTimeString()}</h2>
+        <h3>{count}</h3>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   )
 }
